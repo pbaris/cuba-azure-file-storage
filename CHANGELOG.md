5f0c9cb001622a08f88b012f604b1c42ebb16ea5
panos <pbaris@gmail.com>
2020-05-27 15:56:35 +0300

    add publish script

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## [1.0.0] - 2020-05-27
+
+- Azure File Storage for CUBA 7.2
---
+++ publish.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+usage()
+{
+    echo
+    echo "usage: ./publish.sh <bintray_api_key> [version] [additional_bintray_params]"
+    echo
+    echo "Publish the package to Bintray, optionally specifying an artifact version."
+    echo "If [version] is not specified, the latest version is published."
+    echo
+}
+
+if [[ -z "$BINTRAY_API_KEY" ]]; then
+    [[ -z "$1" ]] && usage && exit 1
+    BINTRAY_API_KEY=$1
+    shift
+fi
+
+if [[ -n "$1" ]] && [[ ! $1 == -P* ]]; then
+    __VERSION=-Pcuba.artifact.version=$1
+    shift
+fi
+
+./gradlew clean assemble bintrayUpload -PbintrayUser=pbaris -PbintrayApiKey=$BINTRAY_API_KEY $__VERSION $@
