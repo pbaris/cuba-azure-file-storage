47feaeaa9c6ac99ef63502a65f685f2504ec0331
panos <pbaris@gmail.com>
2020-05-27 16:54:46 +0300

    publish

--- modules/core/test/gr/netmechanics/cuba/afs/blob/AzureFileStorageTest.java
+++ modules/core/test/gr/netmechanics/cuba/afs/blob/AzureFileStorageTest.java
@@ -12,7 +12,6 @@ import com.haulmont.cuba.core.global.AppBeans;
 import com.haulmont.cuba.core.global.Configuration;
 import gr.netmechanics.cuba.afs.AzureFileStorageTestContainer;
 import org.apache.commons.io.IOUtils;
-import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
@@ -20,7 +19,6 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 /**
  * @author Panos Bariamis (pbaris)
  */
-@Ignore
 public class AzureFileStorageTest {
     private static final String FILE_CONTENT = "This text is for Azure Storage Blob.";
 
